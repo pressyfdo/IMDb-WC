b1385c14582c42014a5c851202507d93493b785f
Pressy Fdo <pressy@freeth.in>
2017-06-25 13:30:14 +0530

    COnfigured ALLOWED_HOSTS to *

--- imdb_wc/settings.py
+++ imdb_wc/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '047_77-_=3jk#xdt#wujdzax&@tjua*gd2w^6ltvz+uy+npdb8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
